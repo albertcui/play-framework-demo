6992e5014a7c46ec1b9de18e08bd68d2b1d45a9c
Albert Cui <aqc2109@columbia.edu>
2015-09-21 02:54:40 -0400

    try adding db

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,8 +6,12 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import play.db.ebean.*;
+import java.util.*;
 
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
 
@@ -21,5 +25,35 @@ public class Application extends Controller {
     
     public Result dynamic(String name) {
         return ok(a_dynamic_page.render(name));
+    }
+    
+    public Result toDo() {
+        
+        List<ToDo> toDoList;
+        try {
+            toDoList = ToDo.find.all();
+        } catch (Exception e) {
+            toDoList = new LinkedList<ToDo>();
+        }
+        
+        Form<ToDo> toDoForm = Form.form(ToDo.class);
+        return ok(todo.render(toDoForm, toDoList));
+    }
+    
+    public Result submit() {
+        DynamicForm toDoForm = new DynamicForm();
+        
+        toDoForm = toDoForm.bindFromRequest(); //forms are immutable
+
+        if (toDoForm.hasErrors()) {
+            return badRequest("Uh oh");
+        } else {
+            System.out.println(toDoForm.get("description"));
+            System.out.println("---------------------------");
+            (new ToDo(toDoForm.get("description"), toDoForm.get("done") != null ? true : false))
+                .save();
+                
+            return redirect("/todo");
+        }
     }
 }
---
+++ app/models/ToDo.java
@@ -0,0 +1,25 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import play.db.ebean.*;
+
+@Entity
+@Table(name="todo")
+public class ToDo extends Model {
+
+    @Id
+    public Long id;
+    
+    public String description;
+    public boolean done;
+
+    public ToDo(String description, boolean done) {
+        this.description = description;
+        this.done = done;
+    }
+    
+    public static Finder<Long, ToDo> find = new Finder<Long,ToDo>(ToDo.class);
+
+
+}
